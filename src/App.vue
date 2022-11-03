175fe7281ccdad3f7e80e3d47d8ec57c80104685
BigSpaceships <bigspaceships56@gmail.com>
2022-11-02 21:12:43 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+export default {
+    components: {
+        
+    }
+}
+</script>
+
+<template>
+    <header>
+        HELLO
+    </header>
+</template>
+
+<style>
+header {
+    max-height: 100vh;
+    padding: 1em
+}
+</style>
---
+++ src/components/TopHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        Hello
+    </header>
+</template>
+
+<style>
+    header {
+        background-color: darkslategray;
+    }
+</style>
