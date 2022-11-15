ad57180ec0ede12e42a52e58fd9a965e82642541
BigSpaceships <39912424+BigSpaceships@users.noreply.github.com>
2022-11-15 14:19:14 +0000

    CHORE: Added plan to the ItemDetails

--- src/components/ItemDetails.vue
+++ src/components/ItemDetails.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+// name and checkbox up top
+// have a menu with three dots for rename and delete (or maybe move it somewhere else if thats hard)
+// calander for when it was completed if it is completed
+// 
+</script>
+
 <template>
 <div class="item-details-block">
     Hi
