7cf08948131f917b66a09d94bebfb90a657c521f
BigSpaceships <39912424+BigSpaceships@users.noreply.github.com>
2022-11-21 15:37:43 +0000

    FEAT: SERVER WORKS

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TodoListView from "../views/TodoListView.vue"
+import TodoListView from "../views/TodoListView.vue";
 
 const routes = [
   { path: '/', component: TodoListView },
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,16 @@
 import express from "express";
+import history from "connect-history-api-fallback";
+const app = express();
 
+const staticFileMiddleware = express.static(__dirname);
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
 
-export {};
+const port = 5555;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
