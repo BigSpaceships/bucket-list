6af71224e5ec3e703f3ba93165b5545e00ef3389
BigSpaceships <bigspaceships56@gmail.com>
2022-11-29 21:21:05 -0500

    CHORE: Removed unused code

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,16 +2,7 @@ import express from "express";
 import history from "connect-history-api-fallback";
 const app = express();
 
-// const staticFileMiddleware = express.static(__dirname);
-// app.use(staticFileMiddleware);
-// app.use(history({
-//   disableDotRule: true,
-//   verbose: true
-// }));
-// app.use(staticFileMiddleware);
-
 app.get('/api/', (req, res) => {
-    console.log("hi");
     res.send("data");
 })
 
