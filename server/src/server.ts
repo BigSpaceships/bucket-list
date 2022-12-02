f2a271048e0fd4ba104d32866f5ad96f0a0985da
BigSpaceships <39912424+BigSpaceships@users.noreply.github.com>
2022-12-02 19:36:38 +0000

    FEAT: express app only responds to local requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,6 +26,6 @@ app.post('/api/new-todo', (req, res) => {
 })
 
 const port = 3000;
-app.listen(port, () => {
+app.listen(port,'127.0.0.1', () => {
   console.log(`Example app listening on port ${port}!`);
 });
