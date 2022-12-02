b9fc8cbec0e570416aac19852f0c8e6270634060
BigSpaceships <39912424+BigSpaceships@users.noreply.github.com>
2022-12-02 02:35:09 +0000

    CHORE: reinstalled node packages

--- replit.nix
+++ replit.nix
@@ -4,6 +4,7 @@
         pkgs.yarn
         pkgs.esbuild
         pkgs.nodejs-16_x
+        pkgs.concurrently
 
         pkgs.nodePackages.typescript
         pkgs.nodePackages.typescript-language-server
