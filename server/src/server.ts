56d26ed0be9ab68d737ea90ac11b5d998cc48b28
BigSpaceships <bigspaceships56@gmail.com>
2022-12-24 21:23:05 +0000

    CHORE: cleaned up code

--- server/src/server.ts
+++ server/src/server.ts
@@ -63,22 +63,7 @@ expressServer.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 })
 
-
-// console.log(io.eventNames())
-
 async function killServer() {
-    // io.emit("message", "kill"); 
-    // await fetch("https://discord.com/api/webhooks/1056267187326824459/Tkj3SOWYWLGbnnptM-py2-h4wANfBtvH_PPGBe5-SCV8-_PDleyF_DWrHBqTUc3O2Fhn", {
-    //     method: "post",
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({
-    //         content: "hi"
-    //     })
-    // })
-    console.log("stop?");
-
     const closePromise = new Promise((resolve, reject) => {
         io.close(() => {
             resolve(1);
@@ -87,21 +72,7 @@ async function killServer() {
 
     await closePromise;
 
-
     process.exit(0);
 }
 
-// process.on("message", (msg) => {
-//     if (msg == "stop") {
-//         console.log("stopping");
-//         process.exit()
-//     }
-// })
-
-// process.on("SIGINT", killServer)
-
-// process.on("SIGKILL", killServer)
-
 process.on("SIGUSR2", killServer)
-
-// proxyServer.listen(4001)
